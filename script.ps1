d8e6e8b8ebc4720c36cd3caa460297460dc0a025
George Wallace <georgewallace@users.noreply.github.com>
2017-10-23 09:20:24 -0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,5 +1,4 @@
-$client = new-object System.Net.WebClient
-$client.DownloadFile("https://dot.net/v1/dotnet-install.ps1","./dotnet-install.ps1")
+Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -OutFile "./dotnet-install.ps1"
 ./dotnet-install.ps1
 
 Write-host "Installing Posh-Git"
@@ -7,32 +6,32 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -force
 
 
 # Install chocolately 
-$client.DownloadFile('https://chocolatey.org/install.ps1',"./choco-install.ps1")
+Invoke-WebRequest 'https://chocolatey.org/install.ps1' -OutFile "./choco-install.ps1"
 ./choco-install.ps1
 SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install git -y
-SET "PATH=%PATH%;c:\[program files\git\bin"
+SET "PATH=%PATH%;c:\program files\git\bin"
 New-Item -ItemType Directory -Path c:\git -Force
-cd c:\git
+Set-Location c:\git
 Write-host "cloning repo"
 git clone https://github.com/georgewallace/StoragePerfandScalabilityExample
 
 write-host "Changing directory to $((Get-Item -Path ".\" -Verbose).FullName)"
-cd StoragePerfandScalabilityExample
+Set-Location c:\git\StoragePerfandScalabilityExample
 
 Write-host "restoring nuget packages"
 dotnet restore
 dotnet build
 
 New-Item -ItemType Directory d:\perffiles
-cd d:\perffiles
+Set-Location d:\perffiles
 Write-host "cretting files"
 for($i=0; $i -lt 36; $i++)
 {
 $out = new-object byte[] 107374100; 
 (new-object Random).NextBytes($out); 
-[IO.File]::WriteAllBytes(".\test\$([guid]::NewGuid().ToString()).txt", $out)
+[IO.File]::WriteAllBytes(".\$([guid]::NewGuid().ToString()).txt", $out)
 }
 
 Write-Host $error
