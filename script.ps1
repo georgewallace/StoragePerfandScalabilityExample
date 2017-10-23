ebee123c37538684962e3134bac20f0284ec92c6
George Wallace <georgewallace@users.noreply.github.com>
2017-10-23 11:11:09 -0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,6 +1,7 @@
 Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -OutFile "./dotnet-install.ps1" 
 ./dotnet-install.ps1
-SET "PATH=%PATH%;C:\Windows\System32\config\systemprofile\AppData\Local\Microsoft\dotnet"
+$dotnetpath = "C:\Windows\System32\config\systemprofile\AppData\Local\Microsoft\dotnet\"
+
 Write-host "Installing Posh-Git"
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -force
 
@@ -8,21 +9,24 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -force
 # Install chocolately 
 Invoke-WebRequest 'https://chocolatey.org/install.ps1' -OutFile "./choco-install.ps1"
 ./choco-install.ps1
-SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install git -y
-SET "PATH=%PATH%;c:\program files\git\bin"
+$chocopath = "C:\Documents and Settings\All Users\chocolatey\bin\"
+
+Invoke-Expression -Command "$($chocopath)choco.exe" install git -y
+
+$gitpath = "c:\program files\git\bin\"
+
 New-Item -ItemType Directory -Path c:\git -Force
 Set-Location c:\git
 Write-host "cloning repo"
-git clone https://github.com/georgewallace/StoragePerfandScalabilityExample
+Invoke-Expression -Command "$($gitpath)git.exe" clone https://github.com/georgewallace/StoragePerfandScalabilityExample
 
 write-host "Changing directory to $((Get-Item -Path ".\" -Verbose).FullName)"
 Set-Location c:\git\StoragePerfandScalabilityExample
 
 Write-host "restoring nuget packages"
-dotnet restore
-dotnet build
+Invoke-Expression -Command "$($dotnetpath)dotnet.exe" restore
+Invoke-Expression -Command "$($dotnetpath)dotnet.exe" build
 
 New-Item -ItemType Directory d:\perffiles
 Set-Location d:\perffiles
